8893748344bbb8dc7ca648e528f9417656cccce0
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2020-10-09 12:30:29 +0100

    added sample main

---
+++ __main__.py
@@ -0,0 +1,57 @@
+from .special_game import special_game
+from .util import load_countries, load_probs
+from .human_agent import human_agent
+from .random_agent import random_agent
+from .logger import logger
+from .timer import turn_timer
+
+from random import seed
+from os import listdir
+seed(124)
+
+n_agents = 6
+
+import time
+
+#PRE the agents and game have already been initialised
+def play(agents, logging, output=None):
+    done = False
+    i = 0
+    turns = 0
+    while not done:
+        try:
+            agent = agents[i]
+            action = agent.step()
+            done = agent.game.take_action(action, i)
+            
+            
+            if output is not None:
+                output.send(action, i)
+            
+            agent.game.timer.tick()
+            i += 1
+            turns += 1
+            i %= len(agents)
+            print(agent.game.players)
+            logging.log(agent.game.state)
+        except KeyboardInterrupt:
+            if input("Save game? (Y | N)\n").upper() == "N":
+                exit(0)
+            return
+
+from game_utils.connection.conquer import conquer
+# time.sleep(2)
+if __name__ == "__main__":
+    probs = load_probs("combos.txt")
+    # g = simple_game(load_countries("./minimax/map.txt"), n_agents)
+    t = turn_timer(60)
+    g = special_game(load_countries("game_utils/conquer_map.txt"), n_agents, t, 1, 10)
+    c = conquer(g)
+    log = logger()
+    # agents = [minimax_agent(g, 0, 2, probs)] + [random_agent(g, i) for i in range(1, n_agents)]
+    c.init_game()
+
+    play(agents, log, c)
+    n = len(listdir("./games"))
+    log.flush(f"./games/minimax{n}")
+    print(f"saved game as minimax{n}")
