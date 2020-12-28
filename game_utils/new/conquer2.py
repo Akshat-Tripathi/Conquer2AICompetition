865c9e279d5f93e0bce73aff7c9a2a11b3e36b5d
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2020-12-28 17:10:48 +0000

    Added message handling

--- game_utils/new/conquer2.py
+++ game_utils/new/conquer2.py
@@ -18,24 +18,22 @@ class update_message:
 def make_action(troops: int, action_type: str, src: str, dest: str):
     return f"{{\"Troops\": {troops}, \"ActionType\": \"{action_type}\", \"Src\": \"{src}\", \"Dest\": \"{dest}\"}}"
 class conquer2_adapter:
-    event_loop = asyncio.get_event_loop()
     players = []
 
-    owners_lock = asyncio.Lock()
-    owners = {}
     troops_lock = asyncio.Lock()
     troops = 0
     countries_lock = asyncio.Lock()
-    countries = {}
+    countries = []
 
     send_lock = asyncio.Lock()
     action = None #The message to be sent
     sent = asyncio.Semaphore(value=1)
 
-    def __init__(self, username: str, password: str, code: str):
+    def __init__(self, username: str, password: str, code: str, tokens):
         self.username = username
         self.password = password
         self.code = code
+        self.country_name_to_idx = dict([(tokens[i], i) for i in range(len(tokens))])
 
         self.start_event = asyncio.Event()
         self.end_event = asyncio.Event()
@@ -52,42 +50,43 @@ class conquer2_adapter:
             while True:
                 await self._send(websocket)
                 await self._recv(websocket)
+                if await self.end_event.wait():
+                    break
 
     async def _send(self, websocket):
-        # if self.exit
         #send a message if one's ready
         with self.send_lock:
             if self.action != None:
                 await websocket.send(self.action)
-                self.sent.release() 
+                self.sent.release()
 
     async def _recv(self, websocket):
         msg = await websocket.recv()
-        msg = json.loads(msg)
-        if msg.type == "updateCountry":
-            return False
-        elif msg.type == "updateTroops":
-            return False
-        elif msg.type == "readyPlayer":
-            return False
-        elif msg.type == "newPlayer":
-            return False
-        elif msg.type == "start":
-            self.start_event.set()
-            return False
-        elif msg.type == "won":
+        msg = update_message(json.loads(msg))
+        if msg.type == "won":
             self.end_event.set()
             return True
+        if msg.type == "updateCountry":
+            with self.countries_lock:
+                self.countries += [(self.country_name_to_idx[msg.country], msg.troops, msg.player)]
+        elif msg.type == "updateTroops":
+            with self.troops_lock:
+                self.troops += msg.troops
+        elif msg.type == "readyPlayer":
+            pass
+        elif msg.type == "newPlayer":
+            self.players += msg.player
+        elif msg.type == "start":
+            self.start_event.set()
         else:
             raise ValueError("Unknown message received")
+        return False
 
-    def send_command(self, action):
+    async def send_command(self, action):
         ...
 
-    
-    def close(self):
+    async def get_initial_state(self):
         ...
 
-c = conquer2_adapter("Akshat", "asdf", "001f91")
-# c.run()
-print("done?")
+    async def get_state(self):
+        ...
